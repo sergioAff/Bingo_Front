a8dde1056cb07b837f85bf74124465610efb3f8e
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2024-11-11 12:49:14 -0300

    conected back

--- front/lib/auth/authService.ts
+++ front/lib/auth/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_DOMAIN = "http://localhost:8000";
+const BACKEND_DOMAIN = "https://bingo-back.onrender.com/";
 
 const REGISTER_URL = `${BACKEND_DOMAIN}/api/v1/auth/users/`;
 const LOGIN_URL = `${BACKEND_DOMAIN}/api/v1/auth/jwt/create/`;
